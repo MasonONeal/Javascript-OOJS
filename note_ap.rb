1cac899b5fd7e71962ef6acf8e985f9349c4d7fd
Mason O'Neal <masonwid@gmail.com>
2014-11-06 19:25:29 -0800

    Add variables and initial class code

---
+++ note_ap.rb
@@ -0,0 +1,34 @@
+
+### simple notes program
+
+# classes
+
+class NoteList
+
+  def add
+  end
+
+end
+
+
+# initialize variables
+choice = ""
+
+number_of_notes = 0
+notes = []
+notes << ["Test", "here is my note text", "test status"]
+
+notes = NoteList.new
+
+puts notes.inspect
+a = gets.chomp
+
+
+
+
+# start display
+
+puts
+puts "Note Taker"
+puts
+puts "Menu:"
