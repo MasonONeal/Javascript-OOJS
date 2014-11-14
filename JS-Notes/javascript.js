b5216b8502301018454d05f58b7c6ffb2270e80b
Mason O'Neal <masonwid@gmail.com>
2014-11-13 21:31:10 -0800

    Add javascript for variable initilization

--- JS-Notes/javascript.js
+++ JS-Notes/javascript.js
@@ -1,2 +1,13 @@
-// javascript.js
+
+// initialize variables
+
+var chioce = "";
+var numberOfNotes = 0;
+var notes = [];
+var notes = ["Title1", "This is the note text.", "Status1"]
+
+console.log("end of code")
+
+// main program
+
 
