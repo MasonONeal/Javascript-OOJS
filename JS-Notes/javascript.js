3f04dce23e25bed54640bb4f7172e2b8d20cc908
Mason O'Neal <masonwid@gmail.com>
2014-11-13 22:07:01 -0800

    Add button events

--- JS-Notes/javascript.js
+++ JS-Notes/javascript.js
@@ -6,8 +6,25 @@ var numberOfNotes = 0;
 var notes = [];
 var notes = ["Title1", "This is the note text.", "Status1"]
 
-console.log("end of code")
+console.log("end of init")
+
+// functions
+var listAllNotes = function () {
+  console.log("test funct");
+  $( "#textNoteList" ).append( "<p>Test</p>" );
+}
 
 // main program
 
-
+$(function() {
+  $( "#listNotes" ).click(function() {
+      console.log( "List notes" );
+      listAllNotes();
+  });
+  $( "#addNote" ).click(function() {
+      console.log( "Add a note" );
+  });
+  $( "#deleteNotes" ).click(function() {
+      console.log( "Delete a note" );
+  });
+});
